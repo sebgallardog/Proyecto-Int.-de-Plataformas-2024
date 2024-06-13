2976d2528688ffd28a4f49471469866307c63a0e
sebgallardog <106987930+sebgallardog@users.noreply.github.com>
2024-06-13 02:54:42 -0400

    Todo Ok pero no sé retornar un 200

--- main.py
+++ main.py
@@ -325,6 +325,31 @@ def eliminar_producto():
         print(e)
 
 
+@app.route('/update_stock', methods=['POST'])
+def update_stock():
+    e="D"
+    print(e)
+    try:
+        form = request.json['carrito']
+        print(form)
+        conn = mysql.connect
+        cursor = conn.cursor()
+        for item in form:
+            id_producto = item["idProducto"]
+            cantidad = int(item["cantidad"])
+            query = f"SELECT Stock FROM producto WHERE idProducto={id_producto}"
+            cursor.execute(query)
+            emp_row = cursor.fetchone()
+            stock = int(emp_row["Stock"]) - cantidad
+            query = f"UPDATE producto SET Stock={stock} WHERE idProducto={id_producto}"
+            cursor.execute(query)
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(e)
+
+
 @app.errorhandler(404)
 def not_found(error=None):
     message = {
@@ -337,5 +362,5 @@ def not_found(error=None):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
     
--- transbankapi.py
+++ transbankapi.py
@@ -11,11 +11,11 @@ def procesar_pago():
     return redirect(url_for('pagarwebpay', amount=amount))
 
 
-@app.route('/pagarwebpay', methods=['GET'])
+@app.route('/pagarwebpay', methods=['post'])
 def pagarwebpay():
     try:
         host_url = request.host_url
-        amount = request.args['amount']
+        amount = request.form['amount']
         buy_order = 'order12345'
         session_id = 'session12345'
         return_url = f'{host_url}retorno'
@@ -23,15 +23,30 @@ def pagarwebpay():
         redirect_url = f"{response['url']}?token_ws={response['token']}"
         return redirect(redirect_url)
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return e
 
 
 @app.route('/retorno', methods=['GET'])
 def retorno_webpay():
     try:
+        print("hola")
+        host_url = request.host_url
         token = request.args.get('token_ws')
         response = transaction.commit(token)
         res = jsonify(response).json
-        return render_template('resultadopago.html', response=res)
+        return render_template('resultadopago.html', response=res, host_url=host_url)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+
+""" 
+VISA                4051 8856 0044 6623 CVV 123     cualquier fecha de expiración 	Genera transacciones aprobadas.
+AMEX                3700 0000 0002 032  CVV 1234    cualquier fecha de expiración 	Genera transacciones aprobadas.
+MASTERCARD 	        5186 0595 5959 0568 CVV 123     cualquier fecha de expiración 	Genera transacciones rechazadas.
+Prepago VISA        4051 8860 0005 6590 CVV 123     cualquier fecha de expiración 	Genera transacciones aprobadas.
+Prepago MASTERCARD 	5186 1741 1062 9480 CVV 123     cualquier fecha de expiración 	Genera transacciones rechazadas.
+Redcompra 	        4051 8842 3993 7763 	        Genera transacciones aprobadas (para operaciones que permiten débito Redcompra)
+Redcompra 	        4511 3466 6003 7060 	        Genera transacciones aprobadas (para operaciones que permiten débito Redcompra)
+Redcompra 	        5186 0085 4123 3829 	        Genera transacciones rechazadas (para operaciones que permiten débito Redcompra)
+"""
